faba615619ba0fde54e68a2bd2169f43661cf578
vivektanpure0 <vivektanpure0@gmail.com>
2021-07-23 17:33:42 +0530

    Update my-script.js

--- JS/my-script.js
+++ JS/my-script.js
@@ -1,3 +1,28 @@
-console.log("Hello World! 4");
-let num = 10;
-console.log(num);
+// HTML 5 CSS 3 JS 6  
+// Edge - engine to run JS - chakra 
+// Chrome - engine to run JS - v8 
+// Node - engine to run JS - v8 
+
+// introduction to JS 
+// console.log("Hello world! 4");
+// let num = 10;
+// console.log(num);
+
+// variable declaration 
+
+// Java -          int num = 10;
+// JavaScript -    let num = 10;
+// JavaScript -    var let const num = 10; 
+
+let num1 = 10;
+console.log(typeof(num1));
+console.log(num1);
+num1 = 'Sonu';
+console.log(typeof(num1));
+console.log(num1);
+num1 = true;
+console.log(typeof(num1));
+console.log(num1);
+
+
+
